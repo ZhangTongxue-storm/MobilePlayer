4a7f0f19eef81e3534805237685c49033fa3be46
Storm-sev <zq329051@outlook.com>
2017-05-20 12:04:34 +0800

    1,2.0 button  chageState for fullScreen and play button

--- app/src/main/java/com/storm/mobileplayer/activity/SystemVideoActivity.java
+++ app/src/main/java/com/storm/mobileplayer/activity/SystemVideoActivity.java
@@ -4,7 +4,12 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.storm.mobileplayer.R;
@@ -16,6 +21,37 @@ public class SystemVideoActivity extends AppCompatActivity {
 
     @BindView(R.id.video_player)
     VideoView videoPlayer;
+    @BindView(R.id.tv_video_name)
+    TextView tvVideoName;
+    @BindView(R.id.iv_battery)
+    ImageView ivBattery;
+    @BindView(R.id.tv_system_time)
+    TextView tvSystemTime;
+    @BindView(R.id.btn_voice)
+    Button btnVoice;
+    @BindView(R.id.sb_voice)
+    SeekBar sbVoice;
+    @BindView(R.id.btn_switch)
+    Button btnSwitch;
+    @BindView(R.id.tv_duration)
+    TextView tvDuration;
+    @BindView(R.id.sb_duration)
+    SeekBar sbDuration;
+    @BindView(R.id.tv_video_time)
+    TextView tvVideoTime;
+    @BindView(R.id.btn_exit)
+    Button btnExit;
+    @BindView(R.id.btn_pre)
+    Button btnPre;
+    @BindView(R.id.btn_pause)
+    Button btnPause;
+    @BindView(R.id.btn_next)
+    Button btnNext;
+    @BindView(R.id.btn_fullscreen)
+    Button btnFullscreen;
+
+    private boolean isPlayer = true;            //当前视频是否播放
+    private boolean isFullScreen = false;       // 当前视频是否全屏
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +60,58 @@ public class SystemVideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_system_video);
         ButterKnife.bind(this);
         setVideoPlayer();
+        setListener();
+
+    }
+
+    private void setListener() {
+
+
+        btnFullscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isFullScreen = !isFullScreen;
+                setScreenState();
+
+            }
+        });
+
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPlayer = !isPlayer;
+                setPlayOrPauseState();
+
+            }
+        });
+
+
+    }
+
+    /**
+     * 设置切换是否全屏的状态
+     */
+    private void setScreenState() {
+        if (isFullScreen)
+            // 全屏状态
+            btnFullscreen.setBackgroundResource(R.drawable.full_screen_selector);
+        else
+            btnFullscreen.setBackgroundResource(R.drawable.unfull_screen_selector);
+
+    }
+
+    /**
+     * 判断播放的状态
+     */
+    private void setPlayOrPauseState() {
+
+        if (isPlayer)
+            // 正在  播放
+            btnPause.setBackgroundResource(R.drawable.pause_selector);
+        else
+            btnPause.setBackgroundResource(R.drawable.player_selector);
+
 
     }
 
