1babdcd4e04c5506442436244384ad656f794158
Storm-sev <zq329051@outlook.com>
2017-05-20 14:34:21 +0800

    1,2.2 监听电池的状态完成

--- app/src/main/java/com/storm/mobileplayer/activity/SystemVideoActivity.java
+++ app/src/main/java/com/storm/mobileplayer/activity/SystemVideoActivity.java
@@ -1,5 +1,9 @@
 package com.storm.mobileplayer.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,17 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.storm.mobileplayer.R;
+import com.storm.mobileplayer.utils.LogUtils;
+import com.storm.mobileplayer.utils.TimeUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SystemVideoActivity extends AppCompatActivity {
+
+    public static final String TAG = "SystemVideoActivity";
 
     @BindView(R.id.video_player)
     VideoView videoPlayer;
@@ -52,27 +59,97 @@ public class SystemVideoActivity extends AppCompatActivity {
 
     private boolean isPlayer = true;            //当前视频是否播放
     private boolean isFullScreen = false;       // 当前视频是否全屏
+    private TimeUtils timeUtils;
+    private BroadcastReceiver batteryReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_system_video);
         ButterKnife.bind(this);
+        initData();
         setVideoPlayer();
         setListener();
 
     }
 
+    private void initData() {
+        Uri uri = getIntent().getData();
+        videoPlayer.setVideoURI(uri);
+        timeUtils = new TimeUtils();
+
+        // 设置电池的状态
+        setBatteryState();
+        int duration = videoPlayer.getDuration();
+        LogUtils.d("视屏的总时长" + duration);
+
+        tvVideoTime.setText(timeUtils.stringForTime(duration));
+
+    }
+
+    /**
+     * 设置电池的状态
+     */
+    private void setBatteryState() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        batteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                int level = intent.getIntExtra("level", 0);
+                setvideoBatterState(level);
+
+            }
+        };
+
+        registerReceiver(batteryReceiver, intentFilter);
+
+    }
+
+    /**
+     * 设置电池的状态
+     *
+     * @param level
+     */
+    private void setvideoBatterState(int level) {
+
+        if (level <= 0) {
+            ivBattery.setImageResource(R.drawable.ic_battery_0);
+        } else if (level > 0 && level <= 10) {
+            ivBattery.setImageResource(R.drawable.ic_battery_10);
+        } else if (level > 10 && level <= 20) {
+            ivBattery.setImageResource(R.drawable.ic_battery_20);
+        } else if (level > 20 && level <= 40) {
+            ivBattery.setImageResource(R.drawable.ic_battery_40);
+        } else if (level > 40 && level <= 60) {
+            ivBattery.setImageResource(R.drawable.ic_battery_60);
+
+        } else if (level > 60 && level <= 80) {
+            ivBattery.setImageResource(R.drawable.ic_battery_80);
+
+        } else if (level > 80 && level <= 100) {
+            ivBattery.setImageResource(R.drawable.ic_battery_100);
+
+        }
+
+    }
+
     private void setListener() {
 
-
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoPlayer.stopPlayback();
+                finish();
+            }
+        });
         btnFullscreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isFullScreen = !isFullScreen;
                 setScreenState();
-
             }
         });
 
@@ -82,7 +159,6 @@ public class SystemVideoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 isPlayer = !isPlayer;
                 setPlayOrPauseState();
-
             }
         });
 
@@ -93,12 +169,14 @@ public class SystemVideoActivity extends AppCompatActivity {
      * 设置切换是否全屏的状态
      */
     private void setScreenState() {
-        if (isFullScreen)
+
+        if (isFullScreen) {
             // 全屏状态
             btnFullscreen.setBackgroundResource(R.drawable.full_screen_selector);
-        else
-            btnFullscreen.setBackgroundResource(R.drawable.unfull_screen_selector);
 
+        } else {
+            btnFullscreen.setBackgroundResource(R.drawable.unfull_screen_selector);
+        }
     }
 
     /**
@@ -106,20 +184,21 @@ public class SystemVideoActivity extends AppCompatActivity {
      */
     private void setPlayOrPauseState() {
 
-        if (isPlayer)
+        if (isPlayer) {
             // 正在  播放
             btnPause.setBackgroundResource(R.drawable.pause_selector);
-        else
+            videoPlayer.start();
+        } else {
             btnPause.setBackgroundResource(R.drawable.player_selector);
+            videoPlayer.pause();
 
+        }
 
     }
 
     private void setVideoPlayer() {
-        Uri uri = getIntent().getData();
 
-        videoPlayer.setVideoURI(uri);
-        videoPlayer.setMediaController(new MediaController(this));
+        // videoPlayer.setMediaController(new MediaController(this));
 
         // 前期准备
         videoPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -145,4 +224,13 @@ public class SystemVideoActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(batteryReceiver);
+        batteryReceiver = null;
+    }
+
+
 }
